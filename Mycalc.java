09ca0aebed3753b2a4c09396f99be8229cbac953
Manikanta <sriramoju.manii1507@gmail.com>
2024-04-27 17:12:15 +0530

    Diff() is added in VScode

--- Mycalc.java
+++ Mycalc.java
@@ -1,11 +1,13 @@
 public class Mycalc
 
 {
-
     int  sum(int a , int b)
     {
         return a+b;
-
+    }
+    int  diff(int a , int b)
+    {
+        return a-b;
     }
 
 
@@ -13,7 +15,7 @@ public class Mycalc
     public static void main(String args[]){
         Mycalc calc = new Mycalc();
         System.out.println("Sum is :"+calc.sum(10,20));
-
+        System.out.println("diff is :"+calc.diff(10,20));
 }
 
 }
